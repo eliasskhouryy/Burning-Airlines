2c270bc190a8fec8e31d28bd37a647a2a125127d
Jonathan Chan <Jonathanchan197@gmail.com>
2022-08-09 23:02:54 +1000

    backend server

--- burning-server/db/seeds.rb
+++ burning-server/db/seeds.rb
@@ -2,8 +2,10 @@ User.destroy_all
 u1 = User.create :user => 'elias', :admin => true
 puts "#{User.count} users"
 
-Flights.destroy_all
+Flight.destroy_all
 f1 = Flight.create :flight_number => 'FLK', :origin => 'Sydney', :destination => 'Korea', :date => 05/05/2020 , :plane => '32'
-puts "#{Flights.count} flights"
+puts "#{Flight.count} flights"
 
 puts "Association"
+u1.flights << f1
+f1.users << u1
